6fceaf5064604a5867ad0e1b1d4339204957e687
Todd Pangilinan <pang.todd@gmail.com>
2022-03-10 16:33:18 -0800

    create first test for UniversalAge, construct a new UniversalAge object with one attribute.

---
+++ __tests__/universalAge.test.js
@@ -0,0 +1,19 @@
+import universalAge from '../src/js/universalAge.js';
+
+describe('universalAge', () => {
+
+test('should correctly create a universalAge object with one attribute', () => {
+    const testUniversalAge = new UniveralAge(24);
+      expect(testUniversalAge.earthAge).toEqual(24);
+}
+//    expect()
+//     expect(livingThingOne.attackPower).toEqual(80);
+//     expect(livingThingOne.agility).toEqual(80);
+//   });
+//   test('should correctly add a random number to the health & attack power', () => {
+//     // const dieRollHealth = new LivingThing.heroStartingStats();
+//     const dieRollHealth = new LivingThing;
+//     dieRollHealth.heroStartingStats();
+//     expect(dieRollHealth.health).toBeLessThan(13);
+//     expect(dieRollHealth.attackPower).toBeLessThan(7);
+//   });
