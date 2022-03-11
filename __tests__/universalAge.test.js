d648a89f28624c722985baeb13e5e49e880a27c8
Todd Pangilinan <pang.todd@gmail.com>
2022-03-11 11:32:51 -0800

    update universalAge.tst.js to include beforeEach to create a single test object for UniversalAge.

--- __tests__/universalAge.test.js
+++ __tests__/universalAge.test.js
@@ -1,23 +1,27 @@
 import UniversalAge from '../src/js/universalAge.js';
 describe('universalAge', () => {
+  let testUniversalAge;
+  beforeEach(()=>{testUniversalAge = new UniversalAge(24);
+  });
 
 test('should correctly create a universalAge object with one attribute', () => {
-    const testUniversalAge = new UniversalAge(24);
       expect(testUniversalAge.earthAge).toEqual(24);
   });
 
   test('should correctly calculate Mercury age based on earth age', ()=> {
-    const testUniversalAge= new UniversalAge(24);
     testUniversalAge.mercuryTime();
       expect(testUniversalAge.mercuryAge).toEqual(5.76);
   });
   test('should correct calculate Venus age based on earth age', ()=> {
-    const testUniversalAge= new UniversalAge(24);
     testUniversalAge.venusTime();
     expect(testUniversalAge.venusAge).toEqual(14.88);
   });
   test('should correct calculate Mars age based on earth age', ()=> {
     const testUniversalAge= new UniversalAge(24);
+    testUniversalAge.marsTime();
+    expect(testUniversalAge.marsAge).toEqual(45.12);
+  });
+  test('should correct calculate Mars age based on earth age', ()=> {
     testUniversalAge.marsTime();
     expect(testUniversalAge.marsAge).toEqual(45.12);
   });
