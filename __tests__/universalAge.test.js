950263a01d85551c4fb56c9e28ef3d3d818e0ba8
Todd Pangilinan <pang.todd@gmail.com>
2022-03-10 18:57:22 -0800

    update universalAge.js to include construtor which passes test one of creating a universal age object.

--- __tests__/universalAge.test.js
+++ __tests__/universalAge.test.js
@@ -1,11 +1,13 @@
-import universalAge from '../src/js/universalAge.js';
+import UniversalAge from '../src/js/universalAge.js';
 
 describe('universalAge', () => {
 
 test('should correctly create a universalAge object with one attribute', () => {
-    const testUniversalAge = new UniveralAge(24);
+    const testUniversalAge = new UniversalAge(24);
       expect(testUniversalAge.earthAge).toEqual(24);
-}
+  });
+})
+
 //    expect()
 //     expect(livingThingOne.attackPower).toEqual(80);
 //     expect(livingThingOne.agility).toEqual(80);
@@ -15,5 +17,4 @@ test('should correctly create a universalAge object with one attribute', () => {
 //     const dieRollHealth = new LivingThing;
 //     dieRollHealth.heroStartingStats();
 //     expect(dieRollHealth.health).toBeLessThan(13);
-//     expect(dieRollHealth.attackPower).toBeLessThan(7);
-//   });
+//     expect(dieRollHealth.attackPower).toBeLessThan(7)
---
+++ src/js/universalAge.js
@@ -0,0 +1,14 @@
+
+export default class UniversalAge {
+  constructor(earthAge) {
+    this.earthAge = earthAge
+  }
+
+  // heroStartingStats() { //FIND HOW TO CALL ON THIS FUNCTION AND SHOVE VALUES INTO A const hero = new LivingThing()
+  //   this.health = diceRoll() * 2
+  //   this.attackPower = diceRoll()
+  //   this.battleHealth = this.health
+  //   this.agility = diceRoll()
+  // };
+
+  }
