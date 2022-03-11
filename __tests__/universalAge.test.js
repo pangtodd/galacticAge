a9d13f503a489e0f2c2a87ae87f0d24960ee341b
Todd Pangilinan <pang.todd@gmail.com>
2022-03-10 19:39:29 -0800

    establish test three for universalAge, to calculate someone's earth age to Venus age.

--- __tests__/universalAge.test.js
+++ __tests__/universalAge.test.js
@@ -6,19 +6,14 @@ test('should correctly create a universalAge object with one attribute', () => {
       expect(testUniversalAge.earthAge).toEqual(24);
   });
 
-  test('should correctly calculate how many earth years equal a year on Mercury', ()=> {
-    const testUniversalAge= new UniversalAge(24;);
-      expect(testUniversalAge.mercuryAge()=5.76);
+  test('should correctly calculate Mercury age based on earth age', ()=> {
+    const testUniversalAge= new UniversalAge(24);
+    testUniversalAge.mercuryAge();
+      expect(testUniversalAge.mercuryAge=5.76);
+  });
+  test('should correct calculate Venus age based on earth age', ()=> {
+    const testUniversalAge= new UniversalAge(24);
+    testUniversalAge.venusAge();
+    expect(testUniversalAge.mercuryAge=14.88);
   });
 })
-
-//    expect()
-//     expect(livingThingOne.attackPower).toEqual(80);
-//     expect(livingThingOne.agility).toEqual(80);
-//   });
-//   test('should correctly add a random number to the health & attack power', () => {
-//     // const dieRollHealth = new LivingThing.heroStartingStats();
-//     const dieRollHealth = new LivingThing;
-//     dieRollHealth.heroStartingStats();
-//     expect(dieRollHealth.health).toBeLessThan(13);
-//     expect(dieRollHealth.attackPower).toBeLessThan(7)
