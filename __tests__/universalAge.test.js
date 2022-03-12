c8c1bcf2f77ab4b67485f014016dff7b460f3ab1
Todd Pangilinan <pang.todd@gmail.com>
2022-03-11 16:10:05 -0800

    wrote test 6, for lifeExpectency function. Also wrote all permutation in test.

--- __tests__/universalAge.test.js
+++ __tests__/universalAge.test.js
@@ -8,8 +8,8 @@ test('should correctly create a universalAge object with one attribute', () => {
       expect(testUniversalAge.earthAge).toEqual(24);
       expect(testUniversalAge.gender).toEqual("male");
       expect(testUniversalAge.smoker).toEqual("no");
-      expect(testUniversalAge.smoker).toEqual("healthy");
-      expect(testUniversalAge.lifeExpectency).toEqual(76);
+      expect(testUniversalAge.lifeStyle).toEqual("healthy");
+      expect(testUniversalAge.lifeExp).toEqual(76);
   });
 
   test('should correctly calculate Mercury age based on earth age', ()=> {
@@ -24,6 +24,32 @@ test('should correctly create a universalAge object with one attribute', () => {
   });
   test('should correct calculate Jupiter age based on earth age', ()=> {
     expect(testUniversalAge.jupiterTime(testUniversalAge.earthAge)).toEqual(284.64);
+  });
+
+  test('should correct calculate life expectency based on gender, smoking status, and lifestyle', ()=> {
+    let smokingHealthyDude = new UniversalAge(24, "male", "yes", "healthy");
+    let smokingLazyDude = new UniversalAge(80, "male", "yes", "no");
+    let noSmokeLazyDude = new UniversalAge(24, "male", "no", "no");
+    let smokingHealthyLady = new UniversalAge(24, "female", "yes","healthy");
+    let smokingLazyLady = new UniversalAge(80, "female", "yes", "no");
+    let noSmokeHealthyLady= new UniversalAge (24, "female", "no", "healthy");
+    let noSmokeLazyLady = new UniversalAge (24, "female", "no", "no");
+    testUniversalAge.lifeExpectency();
+    smokingHealthyDude.lifeExpectency();
+    smokingLazyDude.lifeExpectency();
+    noSmokeLazyDude.lifeExpectency();
+    smokingHealthyLady.lifeExpectency();
+    smokingLazyLady.lifeExpectency();
+    noSmokeHealthyLady.lifeExpectency();
+    noSmokeLazyLady.lifeExpectency();
+    expect(testUniversalAge.lifeExp).toEqual(83);
+    expect(smokingHealthyDude.lifeExp).toEqual(72);
+    expect(smokingLazyDude.lifeExp).toEqual(66);
+    expect(noSmokeLazyDude.lifeExp).toEqual(76);
+    expect(smokingHealthyLady.lifeExp).toEqual(78);
+    expect(smokingLazyLady.lifeExp).toEqual(71);
+    expect(noSmokeHealthyLady.lifeExp).toEqual(88);
+    expect(noSmokeLazyLady.lifeExp).toEqual(81);
   });
 
 })
