ad82a8c6ebf3e6930dc08268a4a4358b54da4abd
Todd Pangilinan <pang.todd@gmail.com>
2022-03-12 16:47:21 -0800

    broke the tests when I corrected the math in the planetTime methods. Updated the tests and re-tested, everything passes.

--- __tests__/universalAge.test.js
+++ __tests__/universalAge.test.js
@@ -13,17 +13,17 @@ test('should correctly create a universalAge object with one attribute', () => {
   });
 
   test('should correctly calculate Mercury age based on earth age', ()=> {
-    expect(testUniversalAge.mercuryTime(testUniversalAge.earthAge)).toEqual(5.76);
+    expect(testUniversalAge.mercuryTime(testUniversalAge.earthAge)).toEqual(100);
   });
   test('should correct calculate Venus age based on earth age', ()=> {
-    expect(testUniversalAge.venusTime(testUniversalAge.earthAge)).toEqual(14.88);
+    expect(testUniversalAge.venusTime(testUniversalAge.earthAge)).toEqual(38.71);
   });
   test('should correct calculate Mars age based on earth age', ()=> {
     const testUniversalAge= new UniversalAge(24);
-    expect(testUniversalAge.marsTime(testUniversalAge.earthAge)).toEqual(45.12);
+    expect(testUniversalAge.marsTime(testUniversalAge.earthAge)).toEqual(12.77);
   });
   test('should correct calculate Jupiter age based on earth age', ()=> {
-    expect(testUniversalAge.jupiterTime(testUniversalAge.earthAge)).toEqual(284.64);
+    expect(testUniversalAge.jupiterTime(testUniversalAge.earthAge)).toEqual(2.02);
   });
 
   test('should correct calculate life expectency based on gender, smoking status, and lifestyle', ()=> {
